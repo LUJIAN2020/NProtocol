657956256cef0b48cbee3daff44ab9eb84b5ae37
LUJIAN2020 <504439788@qq.com>
2025-09-04 21:27:43 +0800

    add nano

---
+++ NProtocol/Protocols/Nano/Enums/Command.cs
@@ -0,0 +1,24 @@
+﻿namespace NProtocol.Protocols.Nano
+{
+    public enum Command
+    {
+        Mn,
+        ER,
+        @E,
+        @K,
+        @M,
+        WRT,
+        ST, RS,
+        STS, RSS,
+        RD, RDS,
+        RDE,
+        WR,
+        WRS,
+        WRE,
+        WS,
+        WSS,
+        MBS, MWS,
+        MBR, MWR,
+        RDC, BE, URD, UWR
+    }
+}
---
+++ NProtocol/Protocols/Nano/Enums/DataType.cs
@@ -0,0 +1,12 @@
+﻿namespace NProtocol.Protocols.Nano
+{
+    public enum DataType
+    {
+        BIT,
+        U,
+        S,
+        D,
+        L,
+        H
+    }
+}
